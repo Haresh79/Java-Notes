42709baaea749c3fb16827bde3c70ebc51c65432
Haresh Panigrahi <121621310+Haresh79@users.noreply.github.com>
2024-02-13 14:08:50 +0530

    Update operators.java

--- programing fundamentals/operators.java
+++ programing fundamentals/operators.java
@@ -5,29 +5,29 @@ public class operators {
         // Java divides the operators into the following groups:
 
         // Arithmetic operators
-       /* int a=15;
+       int a=15;
         int b=5;
         System.out.println(a%b);
         System.out.println(++a);
         System.out.println(--a);
-        */ 
+        
 
         // Assignment operators
-        /*int a=5;
+        int a=5;
         int b=2;
-        System.out.println(b*=5);*/
+        System.out.println(b*=5);
 
         // Comparison operators
-        /*int a=5;
+        int a=5;
         int b=10;
-        System.out.println(a<=b);*/
+        System.out.println(a<=b);
 
         // Logical operators
-        /*int a=5;
+        int a=5;
         int b=12;
         System.out.println(a<b&&b>a);
         System.out.println(a>b||b>a);
-        System.out.println(!(b>a));*/
+        System.out.println(!(b>a));
         // Bitwise operators
     }
 }
